331caea4301b5713309f25f1b44a8491b3c96c88
OWBrony <97199545+OWBrony@users.noreply.github.com>
2022-06-27 14:55:08 -0600

    Update creation.js
    I added functions for dice rolls, setting stats to a now implemented map for the character. I also added comments to divide the functions into sections.
    I added a function to insert HTML after the page is loaded and an event listener to trigger the function.

--- creation.js
+++ creation.js
@@ -1,4 +1,57 @@
+const character = new Map();
+const manipulate = document.querySelector("#manipulate")
 // This section is for rolling stats
-function rollThree{
-    firstRoll = 
+function rollDice(dieFace){
+    return Math.floor(Math.random() * dieFace);
 }
+
+function rollThree(){
+    firstRoll = rollDice(6);
+    secondRoll = rollDice(6);
+    thirdRoll = rollDice(6);
+    total = firstRoll + secondRoll + thirdRoll;
+    return total
+}
+
+function rollDieTwenty(){
+    return rollDice(20)
+}
+
+function setByThree(){
+    character.set("strength", rollThree());
+    character.set("dexterity", rollThree());
+    character.set("constitution", rollThree());
+    character.set("intelligence", rollThree());
+    character.set("wisdom", rollThree());
+    character.set("charisma", rollThree());
+}
+
+function setByTwenty(){
+    character.set("strength", rollDieTwenty);
+    character.set("dexterity", rollDieTwenty);
+    character.set("constitution", rollDieTwenty);
+    character.set("intelligence", rollDieTwenty);
+    character.set("wisdom", rollDieTwenty);
+    character.set("charisma", rollDieTwenty);
+}
+
+// This is for setting modifiers
+
+// This is for setting equipment
+
+// This is for setting class
+
+// This is for setting spells
+
+// This is for setting race
+
+// This is for dynamically setting the page
+function openStatPage(){
+    const html = `<button id = "hand">Input by hand</button>
+    <button id = "3d6">Roll 3d6</button><button id = "1d20">Roll 1d20</button>
+    <section id = "stat block"></section>`;
+    manipulate.innerHTML = html;
+}
+
+// This is for Listeners
+document.querySelector("#stats").addEventListener("click",openStatPage);
