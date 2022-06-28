674caef72a6addfa3ea771366bfa27ba64e88281
OWBrony <97199545+OWBrony@users.noreply.github.com>
2022-06-28 16:23:19 -0600

    Update creation.js
    I was able to get a multi-layer html update.

--- creation.js
+++ creation.js
@@ -1,5 +1,33 @@
 const character = new Map();
 const manipulate = document.querySelector("#manipulate")
+
+let statActive = false
+let raceActive = false
+let classActive = false
+let gearActive = false
+let spellActive = false
+// This is for setting active event listeners
+function setStatActive(active){
+    statActive = active
+    return
+}
+function setRaceActive(active){
+    raceActive = active
+    return
+}
+function setClassActive(active){
+    classActive = active
+    return
+}
+function setGearActive(active){
+    gearActive = active
+    return
+}
+function setSpellsActive(active){
+    spellActive = active
+    return
+}
+
 // This section is for rolling stats
 function rollDice(dieFace){
     return Math.floor(Math.random() * dieFace);
@@ -47,11 +75,33 @@ function setByTwenty(){
 
 // This is for dynamically setting the page
 function openStatPage(){
-    const html = `<button id = "hand">Input by hand</button>
+    setStatActive(true);
+    setClassActive(false);
+    setRaceActive(false);
+    setGearActive(false);
+    setSpellsActive(false);
+    const html = `<button id = "hand" onclick = "showInputStats()">Input by hand</button>
     <button id = "3d6">Roll 3d6</button><button id = "1d20">Roll 1d20</button>
-    <section id = "stat block"></section>`;
+    <section id = "statBlock"></section>`;
     manipulate.innerHTML = html;
+}
+
+function showInputStats(){
+    const statBlock = document.querySelector("#statBlock")
+    let html = `<ul id = "inputList">
+    <li>Strength: <input id = "strengthInput"></input></li>
+    <li>Dexterity: <input id = "dexterityInput"></input></li>
+    <li>Constitution: <input id = "constitutionInput"></input></li>
+    <li>Intelligence: <input id = "intelligenceInput"></input></li>
+    <li>Wisdom: <input id = "wisdomInput"></input></li>
+    <li>Charisma: <input id = "charismaInput"></input></li>
+    </ul>`;
+    statBlock.innerHTML = html;
 }
 
 // This is for Listeners
 document.querySelector("#stats").addEventListener("click",openStatPage);
+if (statActive.addEventListener == true) {
+
+    document.querySelector("#hand").addEventListener("click",showInputStats);
+}
