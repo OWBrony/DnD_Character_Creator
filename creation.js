2fd799e2367a9eed8d68b72f3ed744ed3733fc8a
OWBrony <97199545+OWBrony@users.noreply.github.com>
2022-06-29 15:42:03 -0600

    Clean up and progress
    I removed the code related to rolling a d20. I also made progress toward adding another way to display stats.

--- creation.js
+++ creation.js
@@ -1,50 +1,42 @@
 const character = new Map();
 const manipulate = document.querySelector("#manipulate")
 
-let statActive = false
-let raceActive = false
-let classActive = false
-let gearActive = false
-let spellActive = false
-// This is for setting active event listeners
-function setStatActive(active){
-    statActive = active
-    return
-}
-function setRaceActive(active){
-    raceActive = active
-    return
-}
-function setClassActive(active){
-    classActive = active
-    return
-}
-function setGearActive(active){
-    gearActive = active
-    return
-}
-function setSpellsActive(active){
-    spellActive = active
-    return
-}
-
 // This section is for rolling stats
 function rollDice(dieFace){
     return Math.floor(Math.random() * dieFace);
 }
 
 function rollThree(){
-    firstRoll = rollDice(6);
-    secondRoll = rollDice(6);
-    thirdRoll = rollDice(6);
-    total = firstRoll + secondRoll + thirdRoll;
-    return total
+    let total = 0
+    let firstRoll = rollDice(6);
+    let secondRoll = rollDice(6);
+    let thirdRoll = rollDice(6);
+    let fourthRoll = rollDice(6)
+    let rolls = [firstRoll,secondRoll,thirdRoll,fourthRoll];
+    rolls = removeLowest(rolls);
+    for (i in rolls){
+        if (i != undefined){
+            total += i;
+        }
+    }
+    return total;
 }
 
-function rollDieTwenty(){
-    return rollDice(20)
+function removeLowest(rolls){
+    let lowest = 80;
+    for (number in rolls){
+        if (rolls[number] <= lowest) {
+            lowest = rolls[number];
+        }
+    }
+    for (i in rolls){
+        if (rolls[i] == lowest){
+            delete rolls[i];
+        }
+    }
+    return rolls;
 }
-
+// This is for setting stats
 function setByThree(){
     character.set("strength", rollThree());
     character.set("dexterity", rollThree());
@@ -52,15 +44,7 @@ function setByThree(){
     character.set("intelligence", rollThree());
     character.set("wisdom", rollThree());
     character.set("charisma", rollThree());
-}
-
-function setByTwenty(){
-    character.set("strength", rollDieTwenty);
-    character.set("dexterity", rollDieTwenty);
-    character.set("constitution", rollDieTwenty);
-    character.set("intelligence", rollDieTwenty);
-    character.set("wisdom", rollDieTwenty);
-    character.set("charisma", rollDieTwenty);
+    showStats()
 }
 
 // This is for setting modifiers
@@ -75,13 +59,8 @@ function setByTwenty(){
 
 // This is for dynamically setting the page
 function openStatPage(){
-    setStatActive(true);
-    setClassActive(false);
-    setRaceActive(false);
-    setGearActive(false);
-    setSpellsActive(false);
     const html = `<button id = "hand" onclick = "showInputStats()">Input by hand</button>
-    <button id = "3d6">Roll 3d6</button><button id = "1d20">Roll 1d20</button>
+    <button id = "4d6" onclick = "setByThree()">Roll 4d6</button>
     <section id = "statBlock"></section>`;
     manipulate.innerHTML = html;
 }
@@ -95,13 +74,23 @@ function showInputStats(){
     <li>Intelligence: <input id = "intelligenceInput"></input></li>
     <li>Wisdom: <input id = "wisdomInput"></input></li>
     <li>Charisma: <input id = "charismaInput"></input></li>
+    </ul>
+    <button id = "inputStatButton" onclick = ></button>`;
+    statBlock.innerHTML = html;
+}
+
+function showStats(){
+    const statBlock = document.querySelector("#statBlock");
+    let html = `<ul id = "generatedStats">
+    <li>Strength: ${character.strength}</li>
+    <li>Dexterity: ${character.dexterity}</li>
+    <li>Constitution: ${character.constitution}</li>
+    <li>Intelligence: ${character.intelligence}</li>
+    <li>Wisdom: ${character.wisdom}</li>
+    <li>Charisma: ${character.charisma}</li>
     </ul>`;
     statBlock.innerHTML = html;
 }
 
 // This is for Listeners
 document.querySelector("#stats").addEventListener("click",openStatPage);
-if (statActive.addEventListener == true) {
-
-    document.querySelector("#hand").addEventListener("click",showInputStats);
-}
